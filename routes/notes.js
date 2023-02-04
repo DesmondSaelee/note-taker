768ac4255b5d4f1fd461fde90fc8e5caf94f803f
Brianna Ligon <desmondsaelee@hotmail.com>
2023-02-03 18:22:53 -0800

    fixed errors in server.js. mostly pathing issues.

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,34 @@
-const express = require('express').Router();
-const db = require('./db/db.json') //if can't reach server look at this pathing.
-
+const router = require('express').Router();
+const db = require('../db/db.json') //if can't reach server look at this pathing.
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helper/fsutils');
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
-tips.get('/', (req, res) => {
-    readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+router.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+router.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newTip = {
+      title,
+      text,
+      notesId: uuidv4(),
+    };
+
+    readAndAppend(newTip, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = router
